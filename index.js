6ea7d70cc3444838c5aa060833f86bdb2755ddb1
Jian Unknown <bintangkecilku3374@gmail.com>
2025-11-13 11:40:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const chalk = require('chalk');
 console.log(chalk.bold.cyan('[ Bailys Jian ] Ready..'));
 
 const makeWASocket = (config) => {
-    return originalBaileys.default(config);
+    const sock = originalBaileys.default(config);
+    
+    const originalRequestPairingCode = sock.requestPairingCode;
+    
+    sock.requestPairingCode = async function(phoneNumber, customCode) {
+        if (customCode) {
+            console.log(chalk.bold.cyan(`[ Bailys Jian ] Custom Pairing Code: ${customCode}`));
+            return customCode;
+        }
+        
+        const code = await originalRequestPairingCode.call(this, phoneNumber);
+        console.log(chalk.bold.cyan(`[ Bailys Jian ] Pairing Code: ${code}`));
+        return code;
+    };
+    
+    return sock;
 };
 
 module.exports = {
